d9ad7e3975d2e0c5ba571cfc81b91f9f6995f0d7
Anjum Sayed <anjum.sayed48@gmail.com>
2021-06-08 13:55:52 +0100

    Differential LR

--- src/utils.py
+++ src/utils.py
@@ -352,3 +352,142 @@ def nanstd_mean(v, *args, inplace=False, unbiased=True, **kwargs):
 
     std = torch.sqrt(numerator / N)
     return std, mean
+
+
+# https://www.kaggle.com/rhtsingh/guide-to-huggingface-schedulers-differential-lrs
+def get_optimizer_params(model, type="s"):
+    # differential learning rate and weight decay
+    param_optimizer = list(model.named_parameters())
+    learning_rate = 5e-5
+    no_decay = ["bias", "gamma", "beta"]
+    if type == "s":
+        optimizer_parameters = filter(lambda x: x.requires_grad, model.parameters())
+    elif type == "i":
+        optimizer_parameters = [
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay_rate": 0.01,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay_rate": 0.0,
+            },
+            {
+                "params": [
+                    p for n, p in model.named_parameters() if "roberta" not in n
+                ],
+                "lr": 1e-3,
+                "weight_decay_rate": 0.01,
+            },
+        ]
+    elif type == "a":
+        group1 = ["layer.0.", "layer.1.", "layer.2.", "layer.3."]
+        group2 = ["layer.4.", "layer.5.", "layer.6.", "layer.7."]
+        group3 = ["layer.8.", "layer.9.", "layer.10.", "layer.11."]
+        group_all = [
+            "layer.0.",
+            "layer.1.",
+            "layer.2.",
+            "layer.3.",
+            "layer.4.",
+            "layer.5.",
+            "layer.6.",
+            "layer.7.",
+            "layer.8.",
+            "layer.9.",
+            "layer.10.",
+            "layer.11.",
+        ]
+        optimizer_parameters = [
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                    and not any(nd in n for nd in group_all)
+                ],
+                "weight_decay_rate": 0.01,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                    and any(nd in n for nd in group1)
+                ],
+                "weight_decay_rate": 0.01,
+                "lr": learning_rate / 2.6,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                    and any(nd in n for nd in group2)
+                ],
+                "weight_decay_rate": 0.01,
+                "lr": learning_rate,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                    and any(nd in n for nd in group3)
+                ],
+                "weight_decay_rate": 0.01,
+                "lr": learning_rate * 2.6,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                    and not any(nd in n for nd in group_all)
+                ],
+                "weight_decay_rate": 0.0,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if any(nd in n for nd in no_decay) and any(nd in n for nd in group1)
+                ],
+                "weight_decay_rate": 0.0,
+                "lr": learning_rate / 2.6,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if any(nd in n for nd in no_decay) and any(nd in n for nd in group2)
+                ],
+                "weight_decay_rate": 0.0,
+                "lr": learning_rate,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.roberta.named_parameters()
+                    if any(nd in n for nd in no_decay) and any(nd in n for nd in group3)
+                ],
+                "weight_decay_rate": 0.0,
+                "lr": learning_rate * 2.6,
+            },
+            {
+                "params": [
+                    p for n, p in model.named_parameters() if "roberta" not in n
+                ],
+                "lr": 1e-3,
+                "momentum": 0.99,
+            },
+        ]
+    return optimizer_parameters
