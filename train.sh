40f7e9b998e107443dec16d08fcc41c4a16f78dd
Anjum Sayed <anjum.sayed48@gmail.com>
2021-06-10 09:00:19 +0100

    More seeds

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ config=${1:-default_run}
 
 slug=$(python -c "from coolname import generate_slug; print(generate_slug(3))")
 
-for seed in 48 123 2021
+for seed in 48 123 2021 2016 1987
 do
     timestamp=$(date +%Y%m%d-%H%M%S)
     for i in $(seq 5)
