a94c816d2242312c6aa58ece7072eb9b42d8546d
Anjum Sayed <anjum.sayed48@gmail.com>
2021-06-16 10:24:48 +0100

    Switch to dimm2

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 COMP_NAME = "commonlitreadabilityprize"
 
 INPUT_PATH = Path(f"/mnt/storage_dimm2/kaggle_data/{COMP_NAME}/")
-OUTPUT_PATH = Path(f"/mnt/storage/kaggle_output/{COMP_NAME}/")
+OUTPUT_PATH = Path(f"/mnt/storage_dimm2/kaggle_output/{COMP_NAME}/")
 CONFIG_PATH = Path(f"/home/anjum/kaggle/{COMP_NAME}/hyperparams.yml")
 MODEL_CACHE = Path("/mnt/storage/model_cache/huggingface")
